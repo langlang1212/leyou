eee3e9bbf6241b6a330b582612d7346c2235f795
langlang1212 <350976906@qq.com>
2019-06-19 23:09:47 +0800

    第四天第四视频

--- leyou/ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
+++ leyou/ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -2,12 +2,12 @@ package com.leyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class LyRegistry {
+@EnableDiscoveryClient
+public class LyItemApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LyRegistry.class,args);
+        SpringApplication.run(LyItemApplication.class,args);
     }
 }
